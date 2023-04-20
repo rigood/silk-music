4d72864fb5927c7d83ece94799a9d8f231e3a2e5
rigood <rigood@naver.com>
2023-04-20 16:32:19 +0900

    commit

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -4,17 +4,17 @@ import "./player.js";
 import "./playlist.js";
 import "./like.js";
 
-const modeToggleBtn = document.getElementById("modeToggleBtn");
-modeToggleBtn.addEventListener("click", toggleModeBtn);
+const toggleThemeBtn = document.getElementById("toggleThemeBtn");
+toggleThemeBtn.addEventListener("click", toggleTheme);
 
-function toggleModeBtn(event) {
+function toggleTheme(event) {
   if (event.target.className === "fa fa-sun") {
-    // light 모드로 변경
-    document.documentElement.setAttribute("data-mode", "light");
-    event.target.className = "fa fa-moon";
+    document.documentElement.setAttribute("data-theme", "light");
+    toggleThemeBtn.className = "fa fa-moon";
+    localStorage.setItem("silk_music_theme", "light");
   } else if (event.target.className === "fa fa-moon") {
-    // dark 모드로 변경
-    document.documentElement.setAttribute("data-mode", "dark");
-    event.target.className = "fa fa-sun";
+    document.documentElement.setAttribute("data-theme", "dark");
+    toggleThemeBtn.className = "fa fa-sun";
+    localStorage.setItem("silk_music_theme", "dark");
   }
 }
